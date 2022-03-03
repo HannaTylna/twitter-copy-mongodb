e53b6058658bd5941109db9caa7aacdf57853a7f
Hanna <hanna.tylna@gmail.com>
2022-03-03 22:33:45 +0100

    added pager with userinfo

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 
 //login handle
-router.get('/login',(req,res)=>{
-    res.render('login');
+router.get("/login",(req,res)=>{
+    res.render("login");
 })
-router.get('/register',(req,res)=>{
-    res.render('register')
+router.get("/register",(req,res)=>{
+    res.render("register")
     })
 //Register handle
-router.post('/login',(req,res,next)=>{
-passport.authenticate('local',{
-    successRedirect : '/userprofile',
-    failureRedirect: '/users/login',
+router.post("/login",(req,res,next)=>{
+passport.authenticate("local",{
+    successRedirect : "/userprofile",
+    failureRedirect: "/users/login",
     failureFlash : true
 })(req,res,next)
 })
   //register post handle
-router.post('/register',(req,res)=>{
+router.post("/register",(req,res)=>{
 const {name,email, password, password2} = req.body;
 let errors = [];
-console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
+console.log(" Name " + name+ " email :" + email+ " pass:" + password);
 if(!name || !password || !password2) {
     errors.push({msg : "Please fill in all fields"})
 }
@@ -34,10 +34,10 @@ if(password !== password2) {
 
 //check if password is more than 6 characters
 if(password.length < 6 ) {
-    errors.push({msg : 'password at least 6 characters'})
+    errors.push({msg : "password at least 6 characters"})
 }
 if(errors.length > 0 ) {
-    res.render('register', {
+    res.render("register", {
         errors : errors,
         name : name,
         email : email,
@@ -48,8 +48,8 @@ if(errors.length > 0 ) {
         User.findOne({name : name}).exec((err,user)=>{
         console.log(user);   
         if(user) {
-            errors.push({msg: 'email already registered'});
-            res.render('register',{errors,name,email,password,password2})  
+            errors.push({msg: "email already registered"});
+            res.render("register",{errors,name,email,password,password2})  
         } else {
         const newUser = new User({
             name : name,
@@ -68,8 +68,8 @@ if(errors.length > 0 ) {
                 newUser.save()
                 .then((value)=>{
                     console.log(value)
-                    req.flash('success_msg','You have now registered!');
-                    res.redirect('/users/login');
+                    req.flash("success_msg","You have now registered!");
+                    res.redirect("/users/login");
                 })
                 .catch(value=> console.log(value));
                     
@@ -77,11 +77,34 @@ if(errors.length > 0 ) {
             }
         })
     }
-})
+});
+
 //logout
-router.get('/logout',(req,res)=>{
-req.logout();
-req.flash('success_msg','Now logged out');
-res.redirect('/users/login'); 
-})
+router.get("/logout", (req, res)  =>  {
+    req.logout();
+    req.flash("success_msg", "Now logged out");
+    res.redirect("/users/login"); 
+});
+
+//update
+// router.post("/update", (req,res) => {
+//     res.render("userupdate")
+// });
+
+// function updateRecord(req, res) {
+//     User.findOne({_id:req.body.id},(err,doc)=>{
+//      //this will give you the document what you want to update.. then 
+//     doc.name = req.body.name; //so on and so forth
+    
+//     // then save that document
+//     doc.save(callback);
+    
+//     });
+    
+// }
+// router.put("/update", (req,res) => {
+//     res.send("Update!");
+// });
+
+
 module.exports  = router;

bb0de0dbb46bd59a6629d34e4cd2b68e8699a302
Hanna <hanna.tylna@gmail.com>
2022-03-07 14:45:46 +0100

    fixed all fields

--- models/user.js
+++ models/user.js
@@ -2,20 +2,27 @@ const mongoose = require("mongoose");
 
 const UserSchema  = new mongoose.Schema({
     name :{
-        type  : String,
-        required : true,
-        unique : true
+        type: String,
+        required: true,
+        unique: true
     },
     email :{
-        type  : String
+        type: String
     },
     password :{
-        type  : String,
-        required : true
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
     },
     img: {
-        type: String,
-    },
+        type: String, 
+        default: "img_default.png"
+    }
 });
 
 const User = mongoose.model('User',UserSchema);

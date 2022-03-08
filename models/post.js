ece96606a185f5ca1a2484e59a62258509dc701d
Hanna <hanna.tylna@gmail.com>
2022-03-08 21:36:29 +0100

    added post model

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const moment = require("moment");
+
+const postSchema = new mongoose.Schema({
+    _creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    content: {
+        type: String,
+        maxLength: 140,
+        required: true
+    },
+    timestamps: { 
+        createdAt: 'created_at', 
+        updatedAt: 'updated_at' 
+    }
+});
+
+const Post = mongoose.model("Post", postSchema);
+exports.Post = Post;

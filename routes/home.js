f4aac63e7cd2fda385f2d389f145fa66afb1ad4b
Hanna <hanna.tylna@gmail.com>
2022-03-09 22:34:28 +0100

    changed routes

--- routes/home.js
+++ routes/home.js
@@ -4,33 +4,18 @@ const router = express.Router();
 const { User } = require("../models/user");
 const { Post } = require("../models/post");
 
-// const requireLogin = (req, res, next) => {
-//     if (req.user) {
-//         next()
-//     } else {
-//         res.sendStatus(401)
-//     }
-// };
 
 router.get("/", async (req, res) => {
-    if(!req.user){
-        res.render("homePage");
-    } else {
-        res.render("userPage", { 
-        // send the user information data to the web page
-            user: req.user
-        });
+    try {
+        //find posts 
+        const posts = await Post.find({})
+        .populate("_creator")
+        .exec();
+    
+        res.render("homePage", { posts });
+    } catch (err) {
+        next(err);
     }
 });
-
-// router.get("/:userId", requireLogin, async (req, res) => {
-//     const userId = req.params.userId;
-//     console.log(req.params);
-//     const user = await User.findOne({ _id: userId });
-//     res.render("userPage", { 
-//         // send the user information data to the web page
-//         user: req.user
-//     });
-// });
 
 module.exports = router;

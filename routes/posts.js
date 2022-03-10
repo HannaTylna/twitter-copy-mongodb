5e5330637bd6601b8135aab74a2aa2c08d3ccd86
Hanna <hanna.tylna@gmail.com>
2022-03-10 23:24:21 +0100

    deleted router.post

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const { User } = require("../models/user");
 const { Post } = require("../models/post");
-
-const requireLogin = (req, res, next) => {  
-    if (req.user) {
-        next()
-    } else {
-        res.sendStatus(401)
-    }
-};
 
 router.get("/", async (req, res) => {
 
@@ -21,35 +11,5 @@ router.get("/", async (req, res) => {
     })
 });
 
-router.post("/", requireLogin, async (req, res, next) => {
-    const user = req.user.name;
-    const { content } = req.body;
-    const date = new Date();
-    const dateString = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-    const image = req.user.img;
-
-    const newPost = new Post({ content , date, user, dateString, image})
-    console.log(newPost);
-    let errors = [];
-    if(!content){
-        errors.push({msg: "* You need to write something"});
-        console.log(errors);
-    }
-    if (content.length > 140){
-        errors.push({msg: "* You can write max 140 letters"});
-        console.log(errors)
-    }
-    if (errors.length > 0){
-        const posts = await Post.find().sort({ date: -1});
-        res.render("userPage.ejs", {
-            errors: errors,
-            posts: posts,
-            user: req.user,
-        })
-    } else {
-        await newPost.save()
-        res.redirect("/user")
-    } 
-});
 
 module.exports = router;

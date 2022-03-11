8e7956e26fe277d75f03f267af58426d5f4a035d
Hanna <hanna.tylna@gmail.com>
2022-03-11 14:38:50 +0100

    changed model

--- models/post.js
+++ models/post.js
@@ -1,19 +1,31 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    content: { type: String, maxLength: 140 },
-    date: { type: Date},
-    user: { type: String },
-    dateString: { type: String },
-    image: { type: String },
-    email: { type: String },
-    firstName: { type: String },
-    lastName: { type: String },
-    
-    // timestamps: { 
-    //     createdAt: 'created_at', 
-    //     updatedAt: 'updated_at' 
-    // }
+
+    content: { 
+        type: String, maxLength: 140 
+    },
+    date: { 
+        type: Date
+    },
+    user: { 
+        type: String 
+    },
+    dateString: { 
+        type: String 
+    },
+    image: { 
+        type: String 
+    },
+    email: { 
+        type: String 
+    },
+    firstName: {
+        type: String 
+    },
+    lastName: { 
+        type: String 
+    }
 });
 
 const Post = mongoose.model("Post", postSchema);

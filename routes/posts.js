a94cdb30eac830d219a118a72c040af17d45ebcc
Hanna <hanna.tylna@gmail.com>
2022-03-12 19:33:18 +0100

    changed date to createdAt

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const { Post } = require("../models/post");
 
 router.get("/", async (req, res) => {
 
-    const posts = await Post.find().sort({ date: -1});
+    const posts = await Post.find().sort({ createdAt: -1});
     res.render("postPage.ejs", {
         posts,
         user: req.user,

e01f5684164b78dd6e8c2cb2d53ab070c4f30cbd
Hanna <hanna.tylna@gmail.com>
2022-03-13 12:37:50 +0100

    changed routes follow/unfollow. Unfollow not work

--- routes/user.js
+++ routes/user.js
@@ -38,7 +38,7 @@ router.get("/", requireLogin, async (req, res) => {
         res.render("userPage.ejs", {
             posts,
             user: req.user,
-
+            
         });
     } else{
         res.redirect("/login");
@@ -159,152 +159,82 @@ router.post("/upload", requireLogin,  upload.single("file"), (req, res, next) =>
     }
 });
 
-// router.post("/:userId", async (req, res) => {
-    
-//     const following = req.user.following;
-//     const newFollow = req.params.userId;
-//     const currentUser = req.user.name;
-    
-//     console.log(following)
-//     console.log(newFollow)
-//     console.log(currentUser)
+router.post ("/:id/follow", requireLogin, async (req, res, next) => {
+    try {
+        const username = req.params.id;
+        const user = await User.findOne({name: username});
+        const id = await user.id;
+        
+        const currentUser = await User.findOne(req.user);
+        const currentId = await currentUser.id;
+        console.log(user, id, currentUser, currentId);
 
-//     following.push(newFollow);
-    
-//     await req.user.save();
+        const posts = await Post.find({user: username})
+            .sort({ createdAt: -1 });
+        const img = await user.img;
+        const firstName = await user.firstName;
+        const lastName = await user.lastName;
+        const email = await user.email;
+        let errors = [];
 
-//     console.log(following)
+        // check that your ID does not match the ID of the user you want to track
+        if (currentId === id) {
+            errors.push({msg : "You cannot monitor yourself"})
+        };
 
-//     res.redirect(`/user/${newFollow}`)
-// });
-
-// router.post("/:userId/follow-user", requireLogin, async (req,res) => {
-
-    // check if the requested user and :user_id is same if same then 
-    // console.log(req.user.id ); //622ceb8dda087c0c93cfe8a0 (Teolina)
-    // console.log(req.params.userId) //Emma
-
-    // if (req.user.id === req.params.userId) {
-    //     return res.status(400).json({ alreadyfollow : "You cannot follow yourself"})
-    // } 
-
-    // User.findById(req.params.userId)
-    //     .then(user => {
-
-    //         // check if the requested user is already in follower list of other user then 
-
-    //         if(user.followers.filter(follower => 
-    //             follower.user.toString() === req.user.id ).length > 0){
-    //             return res.status(400).json({ alreadyfollow : "You already followed the user"})
-    //         }
-
-    //         user.followers.unshift({user:req.user.id});
-    //         user.save()
-    //         User.findOne({ name: req.user.name })
-    //             .then(user => {
-    //                 user.following.unshift({user:req.params.userId});
-    //                 user.save().then(user => res.json(user))
-    //             })
-    //             .catch(err => res.status(404).json({alradyfollow:"you already followed the user"}))
-    //     })
-    //res.redirect(`/user/${newFollow}`)
-// })
-
-router.post ("/follow /:userId", requireLogin, async (req, res) => {
-    const username = req.params.userId;
-    res.redirect("/user/profile");
-    // try {
-    //     const username = req.params.userId;
-
-    //     // check if the ID is valid
-    //     if (! ObjectID.isValid (req.params.id)) {
-    //         return res.status (404).json ({error: 'Invalid id'})
-    //     }
-
-    //     // check that your ID does not match the ID of the user you want to track
-    //     if (res.user._id === req.params.id) {
-    //         return res.status (400).json ({error: 'You cannot monitor yourself'})
-    //     }
-
-    //     // add the user ID you want to track to the next array
-    //     const query = {
-    //         _id: res.user._id,
-    //         next: {$not: {$elemMatch: {$eq: id}}}
-    //     }
-
-    //     const update = {
-    //         $addToSet: {next: id}
-    //     }
-
-    //     const updated = await User.updateOne(request, update)
-
-    //     // add your ID to the array of user subscribers you want to track
-    //     const secondQuery = {
-    //         _id,
-    //         subscribers: {$not: {$elemMatch: {$eq: res.user._id}}}
-    //     }
-
-    //     const secondUpdate = {
-    //         $addToSet: {followers: res.user._id}
-    //     }
-
-    //     const secondUpdated = await User.updateOne(secondQuery, secondUpdate)
-
-    //     if (! updated ||! secondUpdated) {
-    //         return res.status (404) .json ({error: 'Unable to follow this user'})
-    //     }
-
-    //     res.status (200) .json (update)
-    // } catch (error) {
-    //     res.status (400) .send ({error: err.message})
-    // }
+        if(errors.length > 0){
+            res.render("userPosts", {
+                errors: errors,
+                user,
+                posts: posts,
+                username,
+                img,
+                firstName, 
+                lastName,
+                email,
+                currentUser,
+                currentId
+            })
+        } else {
+            if (!currentUser.following.includes(id)) {
+              // viktor get following
+                await user.updateOne({ $push: { followers: currentUser } });
+              //merlin get followers
+                await currentUser.updateOne({ $push: { following: user } });
+                res.redirect(`/user/${username}`);
+            } else {
+                res.redirect(`/user/${username}`);
+            }
+        }
+    } catch (error) {
+        next(error)
+    }
 })
 
-router.patch ("/unfollow /:id", requireLogin, async (req, res) => {
+router.post ("/:id/unfollow", requireLogin, async (req, res, next) => {
     try {
-        const {id} = req.params
-
-        // check if the ID is valid
-        if (! ObjectID.isValid (id)) {
-            return res.status(404).json ({error: 'Invalid id'})
-        }
-
+        const username = req.params.id;
+        const user = await User.findOne({name: username});
+        const id = await user.id;
+        
+        const currentUser = await User.findOne(req.user);
+        const currentId = await currentUser.id;
+        console.log(user, id, currentUser, currentId);
+        let errors = [];
         // check that your ID does not match the ID of the user you want to unsubscribe from
-        if (res.user._id === id) {
-            return res.status(400).json ({error: 'You cannot unsubscribe'})
+        if (currentId === id) {
+            errors.push({msg : "You cannot unsubscribe"})
         }
-
-        // delete the user ID you want to unsubscribe from from the following array
-        const query = {
-            _id: res.user._id,
-            next: {$elemMatch: {$eq: id}}
+        if (currentUser.following.includes(id)) {
+            await user.updateOne({ $pull: { followers: currentId } });
+            await currentUser.updateOne({ $pull: { following: id } });
+            res.redirect(`/user/${username}`);
+        } else {
+            res.redirect(`/user/${username}`);
         }
-
-        const update = {
-            $pull: {next: id}
-        }
-
-        const updated = await User.updateOne (request, update)
-
-        // delete your ID from the array of subscribers of the user you want to unsubscribe from
-        const secondQuery = {
-            _id,
-            subscribers: {$elemMatch: {$eq: res.user._id}}
-        }
-
-        const secondUpdate = {
-            $pull: {followers: res.user._id}
-        }
-
-        const secondUpdated = await User.updateOne (secondQuery, secondUpdate)
-
-        if (! updated ||! secondUpdated) {
-            return res.status (404) .json ({error: 'Unable to unsubscribe from this user'})
-        }
-
-        res.status (200) .json (update)
     } catch (error) {
-        res.status (400) .send ({error: err.message})
+        next(error)
     }
 });
+
 module.exports = router;
